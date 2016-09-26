73b0b409f69c29adfd8cef519314c7e0bc1e37a6
Ramirez Nieto Raul Armando <raul.rn3@hotmail.com>
2016-09-26 08:26:43 -0500

    Update cajerin.java

--- cajerin.java
+++ cajerin.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cajero;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 /**
  *
- * @author raulr_000
+ * @author Emmanuel Oropeza
  */
-public class cajerin {
+public class Cajerin extends JFrame {
+
+    JPanel panelNumeros, panelOperaciones;
+    JTextField pantalla;
+
+    public Cajerin() {
+        super();
+        setSize(500, 300);
+        setTitle("Calculadora Simple");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        JPanel panel = (JPanel) this.getContentPane();
+        panel.setLayout(new BorderLayout());
+
+        JButton btn = new JButton();
+        btn.setForeground(Color.RED);
+        setLayout(null);
+        btn = new JButton("retirar");
+        btn.setBounds(100, 150, 100, 30);
+        add(btn);
+
+        btn.setForeground(Color.RED);
+        setLayout(null);
+        btn = new JButton("abonar");
+        btn.setBounds(200, 150, 100, 30);
+        add(btn);
+
+        btn.setForeground(Color.RED);
+        setLayout(null);
+        btn = new JButton("Crear cuenta");
+        btn.setBounds(300, 150, 100, 30);
+        add(btn);
+    }
     
+   public void abono()
+   {
+      
+   }
+   
+   public void retiro(){
+       
+   }
 }
